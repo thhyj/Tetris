c9c3b126ad976f021b436c54b58aa2abba12568b
thhyj <276897571@qq.com>
2020-04-30 17:35:55 +0800

    吐了

--- Tetris.sh
+++ Tetris.sh
@@ -1,17 +1,20 @@
  #ret这个变量用来表示函数的返回值
 ret=0 
  #map用来维护界面的信息
-O={1,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0}
-I={1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0}
-S={0,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0}
-Z={1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0}
-L={1,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0}
-J={0,1,0,0,0,1,0,0,1,1,0,0,0,0,0,0}
-T={1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0}
+O=(1 1 0 0 1 1 0 0 0 0 0 0 0 0 0 0)
+I=(2 2 2 2 0 0 0 0 0 0 0 0 0 0 0 0)
+S=(0 3 3 0 3 3 0 0 0 0 0 0 0 0 0 0)
+Z=(4 4 0 0 0 4 4 0 0 0 0 0 0 0 0 0)
+L=(5 0 0 0 5 0 0 0 5 5 0 0 0 0 0 0)
+J=(0 6 0 0 0 6 0 0 6 6 0 0 0 0 0 0)
+T=(7 7 7 0 0 7 0 0 0 0 0 0 0 0 0 0)
+box=(O I S Z L J T)
 #now表示正在操作的方块的4*4矩阵状态
 now=$O
 #pre表示正在操作的方块的4*4矩阵在上一时刻的状态
 pre=$O
+prex=0
+prey=0
 #-1代表边框
 
 #俄罗斯方块主界面的行列位置
@@ -41,14 +44,30 @@ function Assign() {
     map[$ret]=$v
 }
 function Draw() {
+  #  clear
     for((i = 0; i < 30; ++i)) do 
         for(( j = 0; j < 30; ++j)) do
             Decode $i $j
+  #          echo ${map[$ret]}
             case "${map[$ret]}" in
                 "0")
                     echo -ne "\e[1;33;40m   \e[0m";;
                 "-1")
                     echo -ne "\e[1;33;44m   \e[0m";;
+                "1")
+                    echo -ne "\e[1;33;42m   \e[0m";;
+                "2")
+                    echo -ne "\e[1;33;41m   \e[0m";;
+                 "3")
+                    echo -ne "\e[1;33;43m   \e[0m";;   
+                "4")
+                    echo -ne "\e[1;33;45m   \e[0m";;
+                    "5")
+                    echo -ne "\e[1;33;46m   \e[0m";;
+                "6")
+                    echo -ne "\e[1;33;47m   \e[0m";;
+                    "7")
+                    echo -ne "\e[1;33;42m   \e[0m";;
             esac
         done
             echo ""
@@ -61,13 +80,17 @@ function Add() {
     for((i = x ; i < x + 4 ; ++i)) do
         for((j = y; j < y + 4; ++j)) do
             Trans $((i - x)) $((j - y))
-            echo "ret =  $ret"
+    #        echo "ret =  $ret"
             temp=${now[$ret]}
-            echo "temp = $temp"
+     #       echo "temp = $temp"
             Decode $i $j
-            map[$ret]=temp
+      #      echo "i = $i, j = $j, ret = $ret"
+            map[$ret]=$temp
         done
     done
+    pre=(${now[*]})
+    prex=$x
+    prey=$y
 }
 #把正在操作的方块上一个时刻在的位置擦掉
 function Del() {
@@ -76,8 +99,8 @@ function Del() {
     for((i = x ; i < x + 4 ; ++i)) do
         for((j = y; j < y + 4; ++j)) do
             Trans $((i - x)) $((j - y))
-            temp=$now[$ret]
-            if ((temp==1)) 
+            temp=${now[$ret]}
+            if ((temp>=1)) 
             then   
                 Decode $i $j
                  map[$ret]=0
@@ -105,13 +128,14 @@ function Init() {
     Assign $((rowbegin-1)) $((colend)) -1
     Assign $((rowend)) $((colbegin-1)) -1
     Assign $((rowend)) $((colend)) -1
-    Draw
+#    Draw
 }
 
 Init
-#now=$O
-#echo $now
-#Add 3 10
-#Draw
+now=(${O[*]})
+Add 3 15
 
+Draw
+Del $prex $prey
+Draw
 
